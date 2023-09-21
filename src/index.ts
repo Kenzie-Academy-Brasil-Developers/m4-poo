957afbbf8e9c0b5f0e04a51d20a5e28c6877fab3
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-21 15:03:26 -0300

    constructor

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,26 @@
 // class - encapsula
 
 class Person{
-    name: string = "Alex";
+    name: string;
 
-    sayHello(message: string){
+    constructor(name: string){
+        this.name = name;
+    }
+
+    saySomething(message: string){
         console.log(message);
+    }
+
+    sayMyName(){
+        console.log(`Me chamo ${this.name}`);
     }
 }
 
-const person = new Person();
-console.log(person.name);
-person.sayHello("Olá pessoal, me chamo Alex!");
+const person1 = new Person("Alex");
+
+person1.saySomething("Olá pessoal!");
+person1.sayMyName();
+
+const person2 = new Person("Osvaldo");
+
+person2.sayMyName();
