fb75adb5d254a29d6a4c7940151f89db9d6ea899
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-25 17:21:52 -0300

    extends

--- src/class.ts
+++ src/class.ts
@@ -16,3 +16,22 @@ export class Person{
         console.log(`Me chamo ${this.name}`);
     }
 }
+
+// Herança
+
+export class Student extends Person{
+    module: string;
+
+    constructor(name: string, module: string){
+        super(name);
+        this.module = module;
+    }
+
+    sayMyModule(){
+        console.log(`Estou matriculado no módulo: ${this.module}`);
+    }
+}
+
+// Polimorfismo
+
+export class Teacher extends Person {}
